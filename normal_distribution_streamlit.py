5cd2acecf2c545fcfc1b823f8daea8f1165a865c
Naman Agrawal <dr.namanagrawal@gmail.com>
2025-04-08 22:04:58 +0530

    Update normal_distribution_streamlit.py

--- normal_distribution_streamlit.py
+++ normal_distribution_streamlit.py
@@ -69,7 +69,7 @@ with st.container():
             help="Adjust the standard deviation (σ) of the distribution"
         )
         
-        st.markdown(f"""##### Current parameters: $\mu$ = {mu:.2f}, $\sigma$ = {sigma:.2f}""")
+        st.markdown(f"""#### Current parameters: $\mu$ = {mu:.2f}, $\sigma$ = {sigma:.2f}""")
     
     with col2:
         # Generate plot
